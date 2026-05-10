25a6fb38f152d7f85e47659160c657a5ef6c2aca
mattheuschaves2920-ops <mattheuschaves2920@gmail.com>
2026-05-10 17:44:08 -0300

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'boi-minas-v1';
+
+const urlsToCache = [
+    '/',
+    '/login',
+    '/static/style.css',
+    '/static/icon-192.png',
+    '/static/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
